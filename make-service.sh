1b360901d14c9420526ee691495bc0947320d1b1
Git-UserDS <91082410+Git-UserDS@users.noreply.github.com>
2022-09-28 02:42:55 +0300

    Update make-service.sh

--- make-service.sh
+++ make-service.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 file_path=`pwd`
 service_path="/etc/systemd/system/$1.service"
-if [ -f "$service_path" ]
+
+if (systemctl -q is-active $1.service)
 then
-    echo "Service file already exists"
-    echo "Stopping service..."
     systemctl stop $1.service
-else
+    echo "Stopping existing service..."
+fi
     touch "$service_path"
     service_contents="
 [Unit]
@@ -19,7 +19,8 @@ ExecStart=/home/$2/PythonApp/venv/bin/flask run
 Restart=always
 [Install]
 WantedBy=multi-user.target"
-    echo "$service_contents" >> "$service_path"
-    systemctl start $1.service
-    systemctl enable $1.service
-fi
+echo "Creating new service..."
+echo "$service_contents" >> "$service_path"
+systemctl start $1.service
+systemctl enable $1.service
+
